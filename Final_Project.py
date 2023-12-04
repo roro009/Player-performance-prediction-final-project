7f11ef779aea64ff4a48c35cbacf0066b718a0b2
roro009 <113787389+roro009@users.noreply.github.com>
2023-12-04 01:52:42 -0500

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -278,11 +278,11 @@ from sklearn.impute import SimpleImputer
 def convert_min_to_float(min_str):
     if pd.isna(min_str):
         return None
-    try:
-        parts = min_str.split(':')
-        return int(parts[0]) + int(parts[1]) / 60
-    except:
-        return None
+        try:
+            parts = min_str.split(':')
+            return int(parts[0]) + int(parts[1]) / 60
+        except:
+            return None
 
 # Load and preprocess the data
 @st.cache
