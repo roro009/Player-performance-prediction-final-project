5fea8e22e30c9e303b4df08d67f08cbb1cff49a6
roro009 <113787389+roro009@users.noreply.github.com>
2023-12-04 01:38:16 -0500

    update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -204,62 +204,62 @@ game_details
 # In[2]:
 
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.impute import SimpleImputer
+#import streamlit as st
+#import pandas as pd
+#import numpy as np
+#from sklearn.model_selection import train_test_split
+#from sklearn.ensemble import RandomForestRegressor
+#from sklearn.metrics import mean_squared_error, r2_score
+#from sklearn.impute import SimpleImputer
 
 # Function to convert 'MIN' to float
-def convert_min_to_float(min_str):
-    if pd.isna(min_str):
-        return None
-    try:
-        parts = min_str.split(':')
-        return int(parts[0]) + int(parts[1]) / 60
-    except:
-        return None
+#def convert_min_to_float(min_str):
+   #if pd.isna(min_str):
+       # return None
+   # try:
+     #   parts = min_str.split(':')
+      #  return int(parts[0]) + int(parts[1]) / 60
+    #except:
+    #    return None
 
 # Streamlit app title
-st.title('Basketball Player Performance Prediction')
+#st.title('Basketball Player Performance Prediction')
 
 # File uploader
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-if uploaded_file is not None:
+#uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+#if uploaded_file is not None:
     # Read data
-    games_data = pd.read_csv(uploaded_file)
+   # games_data = pd.read_csv(uploaded_file)
 
     # Data preprocessing
-    games_data['MIN'] = games_data['MIN'].apply(convert_min_to_float)
+   # games_data['MIN'] = games_data['MIN'].apply(convert_min_to_float)
     # Add additional preprocessing as needed
 
     # Feature Engineering
-    n_games = 5
-    player_avg_stats = games_data.groupby(['PLAYER_ID', 'GAME_ID'])[['MIN', 'FGM', 'FGA', 'REB', 'AST', 'STL', 'BLK', 'TO', 'PF', 'PTS']].rolling(n_games, min_periods=1).mean().reset_index()
-    games_data_merged = games_data.merge(player_avg_stats, on=['PLAYER_ID', 'GAME_ID'], suffixes=('', '_avg'))
-    games_data_merged = games_data_merged.dropna(subset=['PTS_avg'])
+   # n_games = 5
+   # player_avg_stats = games_data.groupby(['PLAYER_ID', 'GAME_ID'])[['MIN', 'FGM', 'FGA', 'REB', 'AST', 'STL', 'BLK', 'TO', 'PF', 'PTS']].rolling(n_games, min_periods=1).mean().reset_index()
+   #games_data_merged = games_data.merge(player_avg_stats, on=['PLAYER_ID', 'GAME_ID'], suffixes=('', '_avg'))
+   # games_data_merged = games_data_merged.dropna(subset=['PTS_avg'])
 
     # Selecting features and target
-    features = games_data_merged[['MIN_avg', 'FGM_avg', 'FGA_avg', 'REB_avg', 'AST_avg', 'STL_avg', 'BLK_avg', 'TO_avg', 'PF_avg']]
-    target = games_data_merged['PTS_avg']
+    #features = games_data_merged[['MIN_avg', 'FGM_avg', 'FGA_avg', 'REB_avg', 'AST_avg', 'STL_avg', 'BLK_avg', 'TO_avg', 'PF_avg']]
+   # target = games_data_merged['PTS_avg']
 
     # Splitting the data
-    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+   # X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 
     # Model Training
-    model = RandomForestRegressor(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
+    #model = RandomForestRegressor(n_estimators=100, random_state=42)
+   # model.fit(X_train, y_train)
 
     # Making predictions and evaluating the model
-    y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+   # y_pred = model.predict(X_test)
+   # mse = mean_squared_error(y_test, y_pred)
+    #r2 = r2_score(y_test, y_pred)
 
     # Displaying the results
-    st.write(f"Mean Squared Error: {mse}")
-    st.write(f"R-squared: {r2}")
+   # st.write(f"Mean Squared Error: {mse}")
+    #st.write(f"R-squared: {r2}")**/
 
 
 # In[3]:
